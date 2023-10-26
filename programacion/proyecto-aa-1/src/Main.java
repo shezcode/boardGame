3bc8a1d64eccb380f44ff343dcf68837c553395b
SHEZ <shezcode@gmail.com>
2023-10-26 13:36:11 +0200

    interactive

--- programacion/proyecto-aa-1/src/Main.java
+++ programacion/proyecto-aa-1/src/Main.java
@@ -1,18 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
-    static char[][] tableroHomer = new char[6][6];
-    static char[][] tableroBart = new char[6][6];
+    private static Scanner scanner = new Scanner(System.in);
+    static char[][] tableroJugador1= new char[6][6];
+    static char[][] tableroJugador2 = new char[6][6];
     public static void main(String[] args) {
         Tablero tablero1 = new Tablero();
         Tablero tablero2 = new Tablero();
-        tablero1.initializeTablero(tableroHomer);
+        tablero1.initializeTablero(tableroJugador1);
         tablero1.generarPosicionJugador();
         tablero1.generarPosicionEnemigos();
-        tablero1.insertPosiciones('H', 'F', tableroHomer);
-        tablero1.printTablero(tableroHomer);
-        tablero2.initializeTablero(tableroBart);
+        char[] datos1 = pedirDatos(1);
+        tablero1.insertPosiciones(datos1[0], datos1[1], tableroJugador1);
+        tablero1.printTablero(tableroJugador1);
+        tablero2.initializeTablero(tableroJugador2);
         tablero2.generarPosicionJugador();
         tablero2.generarPosicionEnemigos();
-        tablero2.insertPosiciones('B', 'K', tableroBart);
-        tablero2.printTablero(tableroBart);
+        char[] datos2 = pedirDatos(2);
+        tablero2.insertPosiciones(datos2[0], datos2[1], tableroJugador2);
+        tablero2.printTablero(tableroJugador2);
+    }
+
+    static char[] pedirDatos(int jugador){
+        System.out.printf("Jugador %d, Introduce tu nombre: \n", jugador);
+        char letraJugador = scanner.nextLine().charAt(0);
+        System.out.printf("Jugador %d, Introduce el nombre de tu enemigo: \n", jugador);
+        char letraEnemigo = scanner.nextLine().charAt(0);
+        return new char[]{letraJugador, letraEnemigo};
     }
 }
